63970e611003869028fdb66d83322ddb75b49e1c
kenneth <kokiidoki@gmail.com>
2026-05-06 15:04:41 -0400

    avoid duplicate fctag objs and toggle based on fc status

--- Patch.cs
+++ Patch.cs
@@ -2,6 +2,8 @@ using System.IO;
 using System.Linq;
 using CustomFC;
 using HarmonyLib;
+using Shared;
+using Shared.PlayerData;
 using Shared.TrackSelection;
 using TMPro;
 using UnityEngine;
@@ -24,37 +26,23 @@ public static class CustomTrackSelectionPatch
 {
     [HarmonyPatch(nameof(CustomTrackSelectionOption.SetDifficulty))]
     [HarmonyPostfix]
-    public static void SetDifficulty(CustomTrackSelectionOption __instance)
+    public static void SetDifficulty(CustomTrackSelectionOption __instance, Difficulty selectedDifficulty)
     {
-        if (!CustomFCPlugin.FCObject)
+        Transform FCTagTransform = __instance.transform.Find("BounceContainer/Background/FCTag(Clone)");
+        if (FCTagTransform)
         {
-            GameObject myFCTag = new("FCTag", typeof(RectTransform));
-
-            Image img = myFCTag.AddComponent<Image>();
-            img.sprite = Resources.FindObjectsOfTypeAll<Sprite>().First(sprite => sprite.name == "ComboTag");
-            img.color = new Color(0.0353f, 0.6784f, 1);
-
-            GameObject txtObj = new("FCTag_text", typeof(RectTransform));
-            txtObj.transform.SetParent(myFCTag.transform);
-
-            TextMeshProUGUI tmp = txtObj.AddComponent<TextMeshProUGUI>();
-            tmp.text = "FULL COMBO!";
-            tmp.alignment = TextAlignmentOptions.Center;
-            tmp.horizontalAlignment = HorizontalAlignmentOptions.Center;
-            tmp.font = Resources.FindObjectsOfTypeAll<TMP_FontAsset>().First(font => font.name == "Oxanium-Bold SDF - SoftMaskable");
-            tmp.fontMaterial = Resources.FindObjectsOfTypeAll<Material>().First(mat => mat.name == "Oxanium-Bold SDF - SoftMaskable - DropShadow");
-            tmp.fontSize = 20;
-            tmp.enableAutoSizing = false;
-            tmp.fontStyle = FontStyles.Italic | FontStyles.UpperCase;
-            tmp.color = new Color(1, 1, 1);
-            tmp.margin = new Vector4(40, 5, 0, 0); // Hand tuned!
-
-            CustomFCPlugin.FCObject = myFCTag;
+            GameObject FCTag = FCTagTransform.gameObject;
+            FCTag.SetActive(PlayerDataUtil.GetWasFullClearedByDifficulty(__instance._levelId, selectedDifficulty));
+            return;
         }
+
+        if (!CustomFCPlugin.FCObject)
+            initFCTagGameObj();
 
         Transform background = __instance.transform.Find("BounceContainer/Background");
         GameObject FCInstance = Object.Instantiate(CustomFCPlugin.FCObject);
         FCInstance.transform.SetParent(background);
+        FCInstance.SetActive(false);
         RectTransform rect = FCInstance.GetComponent<RectTransform>();
         rect.pivot = new Vector2(1f, 0f);
         rect.anchorMin = new Vector2(0.991f, 0.9116f);
@@ -63,4 +51,32 @@ public static class CustomTrackSelectionPatch
         rect.localEulerAngles = new Vector3(0f, 0f, 360f);
         rect.anchoredPosition = new Vector2(-0.5136f, -0.93f);
     }
+
+    private static void initFCTagGameObj()
+    {
+        GameObject myFCTag = new("FCTag", typeof(RectTransform));
+        myFCTag.name = "FCTag";
+
+        Image img = myFCTag.AddComponent<Image>();
+        img.sprite = Resources.FindObjectsOfTypeAll<Sprite>().First(sprite => sprite.name == "ComboTag");
+        img.color = new Color(0.0353f, 0.6784f, 1);
+
+        GameObject txtObj = new("FCTag_text", typeof(RectTransform));
+        txtObj.transform.SetParent(myFCTag.transform);
+
+        TextMeshProUGUI tmp = txtObj.AddComponent<TextMeshProUGUI>();
+        tmp.text = "FULL COMBO!";
+        tmp.alignment = TextAlignmentOptions.Center;
+        tmp.horizontalAlignment = HorizontalAlignmentOptions.Center;
+        tmp.font = Resources.FindObjectsOfTypeAll<TMP_FontAsset>().First(font => font.name == "Oxanium-Bold SDF - SoftMaskable");
+        tmp.fontMaterial = Resources.FindObjectsOfTypeAll<Material>().First(mat => mat.name == "Oxanium-Bold SDF - SoftMaskable - DropShadow");
+        tmp.fontSize = 20;
+        tmp.enableAutoSizing = false;
+        tmp.fontStyle = FontStyles.Italic | FontStyles.UpperCase;
+        tmp.color = new Color(1, 1, 1);
+        tmp.margin = new Vector4(40, 5, 0, 0); // Hand tuned!
+
+        CustomFCPlugin.FCObject = myFCTag;
+    }
 }
+
